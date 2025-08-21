d58f3d6023f23790bdf1e20483acb6a1afa45bd4
LELEGOBOO <lelegoboo@gmail.com>
2025-08-21 03:59:34 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "baby-blue": "#89CFF0", // bebek mavisi
+      },
+    },
+  },
+  plugins: [],
+};
