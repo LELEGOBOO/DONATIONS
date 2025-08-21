54dcf7107eb4a077a7a8c2af423c22cd60c9d3ad
LELEGOBOO <lelegoboo@gmail.com>
2025-08-21 03:15:09 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,14 @@
+import Home from "./pages/Home"
 
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-sky-100 to-blue-200">
+      <header className="p-6 text-center bg-sky-500 text-white text-2xl font-bold">
+        Whole 4 World 🌍
+      </header>
+      <Home />
+    </div>
+  )
+}
+
+export default App
