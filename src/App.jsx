70b1e0a0758362d41184812167e1659875b5ada1
LELEGOBOO <lelegoboo@gmail.com>
2025-08-26 03:43:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./pages/Home.jsx";
 
-function App() {
+export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-sky-100 to-blue-200">
       <header className="p-6 text-center bg-sky-500 text-white text-2xl font-bold">
